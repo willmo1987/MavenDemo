c97d7d3f50bf35831d9e8e5e8ecdd481f1ce59d2
Will Mo <willmo1987@gmail.com>
2016-07-05 15:51:21 +0800

    update

---
+++ mavenlib/src/main/java/com/will4it/mavenlib/MavenUtil.java
@@ -0,0 +1,13 @@
+package com.will4it.mavenlib;
+
+/**
+ * author will
+ * date 7/5/16.
+ */
+public class MavenUtil {
+
+    public static String getMavenDesc() {
+        return "This is from Maven Library";
+    }
+
+}
